1102ab43f81f1b50ea45fbc9ab93a2cf7590ce5f
sgawas3 <sgawas3@gatech.edu>
2017-10-07 12:25:30 -0700

    added user creation

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, request, jsonify
 import os
+from os import environ
 import json
 from pymongo import MongoClient
 
-
+db_name = 'heroku_g1h1jbmh'
+user_collection = 'users'
 
 app = Flask(__name__)
+uri = ''
 
-db_name = 'splitpaydb'
-client = None
-db = None
+if "MONGODB_URI" in os.environ:
+    uri = environ.get('MONGODB_URI')
+else:
+    with open('mongo-cred.json') as f:
+        uri = json.load(f)['uri']
 
+client = MongoClient(uri)
 
-
+db = client[db_name]
 
 port = int(os.getenv('PORT', 8000))
 
@@ -21,6 +27,12 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['POST'])
+def create_user():
+    users = db['users']
+    id  = users.insert_one(request.get_json(silent=True)).inserted_id
+    print id
+    return '{user_id :'+str(id)+'}'
 
 
 
