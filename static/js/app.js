0d5f503db1c7b347b11bce2a37029ad529d56abb
Sakshi Pratap <sakshi.s.pratap@oracle.com>
2017-10-07 19:12:31 -0700

    ui changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,33 @@
-//$(document).ready(function() {
-//$(".btn-pref .btn").click(function () {
-//    $(".btn-pref .btn").removeClass("btn-primary")
-//    // $(".tab").addClass("active"); // instead of this do the below
-//    $(this).addClass("btn-primary");
-//});
-//});
+$(document).ready(function() {
+	$(".btn-pref .btn").click(function () {
+	   $(".btn-pref .btn").removeClass("btn-primary")
+	   // $(".tab").addClass("active"); // instead of this do the below
+	   $(this).addClass("btn-primary");
+	});
+
+	$(".add-account").on("click",function(){
+		$(".show-accounts").removeClass("hidden");
+
+	})
+
+	$("#addIdButon").on("click",function(){
+		$(".show-accounts").addClass("hidden");
+		//Ajax call
+		$(".payments-list").prepend('<div><span class="btn btn-sm btn-default col-md-8">#1: Account Ending in '+$("#accountId").val().substring(0,4)+'</span></div>');
+
+
+	})
+
+	$("#paymentSplit").on("click",function(){
+		$(".show-split").removeClass("hidden");
+
+	})
+
+	$("enter").on("click",function(){
+		$(".show-split").append(' <div><div class="col-md-8">Name</div> <input class="col-md-3" type="number" name="limit" class="amount"></div>');
+	})
+
+
+});
+
+
